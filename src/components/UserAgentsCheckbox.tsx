7f1a0716657ebd0bf11274557e8022931c3f8d45
Nalbekink <nalbekink@gmail.com>
2023-03-20 11:36:51 +0000

    change useragents name to uTLS

--- src/components/UserAgentsCheckbox.tsx
+++ src/components/UserAgentsCheckbox.tsx
@@ -1,72 +1,71 @@
 import {
-    HStack,
-    VStack,
-    Checkbox,
-    CheckboxGroup,
-    useCheckbox,
-    Stack,
-    Text,
-    Box,
-    useCheckboxGroup,
-    Tooltip,
-  } from "@chakra-ui/react";
-  
-  import React from "react";
-  import { Grid, GridItem } from "@chakra-ui/react";
+  HStack,
+  VStack,
+  Checkbox,
+  CheckboxGroup,
+  useCheckbox,
+  Stack,
+  Text,
+  Box,
+  useCheckboxGroup,
+  Tooltip,
+} from "@chakra-ui/react";
 
-  interface props {
-    onChange: any;
-  }
-  const UserAgentsCheckbox = ({ onChange }: props) => {
+import React from "react";
+import { Grid, GridItem } from "@chakra-ui/react";
 
-    var useragents = [
-        "chrome",
-        "firefox",
-        "safari",
-        "random",
-        "randomized",
-        "ios",
-        "android",
-        "edge",
-    ];
+interface props {
+  onChange: any;
+}
+const UserAgentsCheckbox = ({ onChange }: props) => {
+  var useragents = [
+    "chrome",
+    "firefox",
+    "safari",
+    "random",
+    "randomized",
+    "ios",
+    "android",
+    "edge",
+  ];
 
-    const getGrid = (max: number) => {
-      let columns = Math.ceil(useragents.length / max)
-      let grid = ``
-      for (let i = 0; i < columns; i++){
-        let row = `"`
-        for (let j = 0; j < max; j++){
-          if (i * max + j >= useragents.length){
-            row += "empty" + ' '
-            continue
-          }
-          row += useragents[i * max + j] + ' '
+  const getGrid = (max: number) => {
+    let columns = Math.ceil(useragents.length / max);
+    let grid = ``;
+    for (let i = 0; i < columns; i++) {
+      let row = `"`;
+      for (let j = 0; j < max; j++) {
+        if (i * max + j >= useragents.length) {
+          row += "empty" + " ";
+          continue;
         }
-        row += `"`
-        grid+= row + " "
+        row += useragents[i * max + j] + " ";
       }
-      return grid
+      row += `"`;
+      grid += row + " ";
     }
-    return (
-      <VStack
-        width="100%"
-        justifyContent="center"
-        alignItems="center"
-        bg="gray.100"
-        p={3}
-        borderRadius={20}
+    return grid;
+  };
+  return (
+    <VStack
+      width="100%"
+      justifyContent="center"
+      alignItems="center"
+      bg="gray.100"
+      p={3}
+      borderRadius={20}
+    >
+      <Tooltip label="your desired fingerprint choices">
+        <Text as="b" p={[2]}>
+          uTLS
+        </Text>
+      </Tooltip>
+      <CheckboxGroup
+        colorScheme="orange"
+        defaultValue={useragents}
+        onChange={(value) => onChange(value)}
       >
-        <Tooltip label="your desired fingerprint choices">
-          <Text as="b" p={[2]}>
-            User Agents 
-          </Text>
-        </Tooltip>
-        <CheckboxGroup
-          colorScheme="orange"
-          defaultValue={useragents}
-          onChange={(value) => onChange(value)}
-        >
-          <Grid
+        <Grid
           templateAreas={{
             base: `${getGrid(2)}`,
             md: `${getGrid(3)}`,
@@ -75,12 +74,20 @@ import {
           p={5}
           width="90%"
         >
-            {useragents.map((ua)=> <GridItem area={ua} alignItems="center" justifyContent="center" key={ua}><Checkbox value={ua}>{ua}</Checkbox></GridItem>)}
-          </Grid>
-        </CheckboxGroup>
-      </VStack>
-    );
-  };
-  
-  export default UserAgentsCheckbox;
-  
+          {useragents.map((ua) => (
+            <GridItem
+              area={ua}
+              alignItems="center"
+              justifyContent="center"
+              key={ua}
+            >
+              <Checkbox value={ua}>{ua}</Checkbox>
+            </GridItem>
+          ))}
+        </Grid>
+      </CheckboxGroup>
+    </VStack>
+  );
+};
+
+export default UserAgentsCheckbox;
