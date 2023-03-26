17afe8340a06b0b20d5c85d1844a58fd6f2d0e2c
Nalbekink <nalbekink@gmail.com>
2023-03-26 17:00:16 +0000

    improve ping algorithm

--- src/services/TestIp.ts
+++ src/services/TestIp.ts
@@ -2,16 +2,19 @@ import axios, { CancelToken, AxiosError } from "axios";
 
 async function testIp(ip: string, timeout: number = 2500, count: number = 5) {
   const url: string = `https://${ip}/`;
-  const startTime = performance.now();
+  let startTime = null;
   let successfulRequests: number = 0;
-  const delay_rate = 1.5;
+  const delay_rate = 1.2;
 
-  for (let i = 0; i < count; i++) {
+  for (let i = 0; i <= count; i++) {
+    if (i == 1) {
+      startTime = performance.now();
+    }
     try {
       const source = axios.CancelToken.source();
       const response = await axios.get(url, {
         cancelToken: source.token,
-        timeout: i == 0 ? 1.5 * delay_rate * timeout : delay_rate * timeout,
+        timeout: i == 0 ? 2 * delay_rate * timeout : delay_rate * timeout,
       });
     } catch (error: any) {
       if (error instanceof AxiosError && error.message.includes("Network")) {
@@ -24,7 +27,7 @@ async function testIp(ip: string, timeout: number = 2500, count: number = 5) {
 
   const duration = (performance.now() - startTime) / delay_rate;
 
-  if (successfulRequests === count) {
+  if (successfulRequests === count + 1) {
     return { ip: ip, time: Math.floor(duration / count) };
   } else {
     return null;
